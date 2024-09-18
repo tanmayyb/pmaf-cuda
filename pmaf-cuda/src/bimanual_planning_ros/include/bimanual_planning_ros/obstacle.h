2265643ddfac9e13959e3aa1c7fe9497ad23f419
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2024-09-18 16:22:35 +0530

    updated obstacle class in obstacle.h

--- pmaf-cuda/src/bimanual_planning_ros/include/bimanual_planning_ros/obstacle.h
+++ pmaf-cuda/src/bimanual_planning_ros/include/bimanual_planning_ros/obstacle.h
@@ -11,32 +11,56 @@
 #include "dqrobotics/interfaces/vrep/DQ_VrepInterface.h"
 #include "eigen3/Eigen/Dense"
 
-namespace ghostplanner {
-namespace cfplanner {
-class Obstacle {
-   private:
-    std::string name_;
-    Eigen::Vector3d pos_;
-    Eigen::Vector3d vel_;
-    double rad_;
+#include <cuda_runtime.h>
 
-   public:
-    Obstacle(const std::string name, const Eigen::Vector3d pos,
-             const Eigen::Vector3d vel, const double rad)
-        : name_{name}, pos_{pos}, vel_{vel}, rad_{rad} {};
-    Obstacle(const Eigen::Vector3d pos, const double rad)
-        : pos_{pos}, rad_{rad}, vel_{0, 0, 0}, name_{""} {};
-    Obstacle(const Eigen::Vector3d pos, const Eigen::Vector3d vel,
-             const double rad)
-        : pos_{pos}, rad_{rad}, vel_{vel}, name_{""} {};
-    Obstacle() : pos_{0, 0, 0}, rad_{0}, vel_{0, 0, 0}, name_{""} {};
-    std::string getName() const { return name_; };
-    Eigen::Vector3d getPosition() const { return pos_; };
-    void setPosition(Eigen::Vector3d pos) { pos_ = pos; }
-    void setVelocity(Eigen::Vector3d vel) { vel_ = vel; }
-    Eigen::Vector3d getVelocity() const { return vel_; };
-    double getRadius() const { return rad_; };
-    void updateVrepObstacles(DQ_VrepInterface &vi, const double delta_t);
-};
-}  // namespace cfplanner
+namespace ghostplanner {
+  namespace cfplanner {
+    class Obstacle {
+      public:
+        std::string name_;
+        Eigen::Vector3d pos_;
+        Eigen::Vector3d vel_;
+        double rad_;
+        double pos_x_,pos_y_,pos_z_;
+        double vel_x_, vel_y_, vel_z_;
+
+      __host__ __device__ Obstacle(): 
+        name_{""}, pos_x_{0}, pos_y_{0}, pos_z_{0}, vel_x_{0}, vel_y_{0}, vel_z_{0}, rad_{0} {};
+
+      __host__ __device__ Obstacle( const std::string name,  const Eigen::Vector3d pos,  const Eigen::Vector3d vel,  const double rad 
+      ): name_{name}, pos_x_{pos.x()}, pos_y_{pos.y()}, pos_z_{pos.z()}, vel_x_{vel.x()}, vel_y_{vel.y()}, vel_z_{vel.z()}, rad_{rad} {};
+
+      __host__ __device__ Obstacle(
+        const Eigen::Vector3d pos, const double rad
+      ): pos_x_{pos.x()}, pos_y_{pos.y()}, pos_z_{pos.z()}, rad_{rad}, vel_x_{0}, vel_y_{0}, vel_z_{0}, name_{""} {};
+      
+      __host__ __device__ Obstacle(
+        const Eigen::Vector3d pos, const Eigen::Vector3d vel, const double rad
+      ): pos_x_{pos.x()}, pos_y_{pos.y()}, pos_z_{pos.z()}, vel_x_{vel.x()}, vel_y_{vel.y()}, vel_z_{vel.z()}, rad_{rad}, name_{""} {};
+      
+      // Vrep Interface
+      __host__   void updateVrepObstacles(DQ_VrepInterface &vi, const double delta_t);
+
+      // getters    
+      __host__ std::string getName() const { return name_; };
+      __host__ Eigen::Vector3d getPosition() const { return pos_; };
+      __host__ Eigen::Vector3d getVelocity() const { return vel_; };
+      __host__ double getRadius() const { return rad_; };
+
+      // setters
+      __host__ void setPosition(Eigen::Vector3d pos) { pos_ = pos; pos_x_ = pos.x(); pos_y_ = pos.y(); pos_z_ = pos.z();}
+      __host__ void setVelocity(Eigen::Vector3d vel) { vel_ = vel; vel_x_ = vel.x(); vel_y_ = vel.y(); vel_z_ = vel.z();}
+
+      // this is that cuda life
+      __host__ __device__ double getPosX(){return pos_x_;}
+      __host__ __device__ double getPosY(){return pos_y_;}
+      __host__ __device__ double getPosZ(){return pos_z_;}
+      __host__ __device__ double getVelX(){return vel_x_;}
+      __host__ __device__ double getVelY(){return vel_y_;}
+      __host__ __device__ double getVelZ(){return vel_z_;}
+      __host__ __device__ double getRad(){return rad_;}
+    };
+  }  // namespace cfplanner
 }  // namespace ghostplanner
+
+
